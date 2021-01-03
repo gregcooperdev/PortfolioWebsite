44984388886e1a3f7dc1de351e2b8abfb66dea71
slothbirdbeard <gregcooper9099@gmail.com>
2021-01-03 21:26:44 +0000

    General updates 03012021

--- main.js
+++ main.js
@@ -4,6 +4,5 @@ document.getElementById('test').style.color = "red";
 
 // jQuery
 $(document).ready(() => {
-  $('.intro').slideDown(1000);
-  $('#me').fadeIn(3500);
+  $('.intro').slideDown(1000); // wait for this to complete
 });
